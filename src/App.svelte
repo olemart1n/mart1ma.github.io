563185338fc343195da3b8bcb4dd83917bc1f608
Ole M <olesnoen@gmail.com>
2023-09-29 01:04:01 +0200

    clear repo

--- src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import Footer from "./lib/Footer.svelte";
-    import Header from "./lib/Header.svelte";
-    import Main from "./lib/Main.svelte";
-</script>
-
-<Header />
-<Main />
-<Footer />
